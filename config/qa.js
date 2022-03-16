08561c9ca7a15488e650729813cd51c5eab1b6be
lakeside763 <lakeside763@gmail.com>
2022-03-16 16:03:12 +0100

    chore: setup configuration for running test

--- config/qa.js
+++ config/qa.js
@@ -1,1 +1,30 @@
-module.exports = {};
+require('dotenv').config();
+const path = require('path');
+
+module.exports = {
+  db: {
+    host: 'localhost',
+    port: 5432,
+    database: 'challenge',
+    user: 'postgres',
+    password: 'password',
+  },
+  debug: {
+    stackSize: 4,
+  },
+  i18next: {
+    translationFilePath: path.resolve(__dirname, '..', 'locales/{{lng}}/{{ns}}.json'),
+  },
+  host: 'localhost:9001',
+  https: {
+    enabled: true,
+  },
+  port: process.env.PORT || 9001,
+  shutdown: {
+    appKill: 1000,
+    serverClose: 100,
+  },
+  auth: {
+    jwtSecret: 'secret',
+  },
+};
